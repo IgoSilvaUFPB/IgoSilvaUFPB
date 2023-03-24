74c4d50db8a6e77d398bd364663de629ff9e18c9
Igó Ferreira Melo Silva <125592146+IgoSilvaUFPB@users.noreply.github.com>
2023-03-24 14:28:36 -0300

    fatorial por recursividade

---
+++ fatorial_por_recursividade.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fat (int num) {
+  	if (num == 0) {
+  		return 1;
+  	} else {
+  		return num*fat(num-1);
+  	}
+}
+
+int main(){
+  int numero;  
+  
+  printf("Qual valor você deseja saber o fatorial: \n");
+  scanf("%d", &numero);
+  
+  printf("O fatorial de %d è %d", numero, fat(numero));
+  return 0;
+}
